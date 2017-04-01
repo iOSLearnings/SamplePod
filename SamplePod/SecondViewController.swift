2e92dfb18e9d731c81b784a16534e715054b41fb
Ponsuyambu <ponsuyambu@Ponsuyambus-iMac.local>
2017-04-01 20:08:07 +0200

    Initial Commit

---
+++ SamplePod/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SamplePod
+//
+//  Created by Ponsuyambu on 01/04/17.
+//  Copyright © 2017 Ponsuyambu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
